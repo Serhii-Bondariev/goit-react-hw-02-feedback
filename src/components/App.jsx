9ea9c27b09dc463cbd9285ee6d164c9869fce650
Serhii-Bondariev <xx.code4u@gmail.com>
2023-11-01 00:56:55 +0200

    refactoring compleate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,58 @@
-import React from 'react';
-import { Feedback } from './Feedback/Feedback';
+// App.jsx
+import React, { Component } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import Section from './Section/Section';
+import { Statistics } from './Statistics/Statistics';
 import ScopedCssBaseline from '@mui/material/ScopedCssBaseline';
 
-export const App = () => {
-  return (
-    <ScopedCssBaseline>
-      <div>
-        <Feedback />
-      </div>
-    </ScopedCssBaseline>
-  );
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = type => {
+    this.setState(prevState => ({ [type]: prevState[type] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return total ? (good / total) * 100 : 0;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <ScopedCssBaseline>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.handleFeedback}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
+      </ScopedCssBaseline>
+    );
+  }
+}
+
+export default App;
